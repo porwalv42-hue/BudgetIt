e32f0d76b313193c3eed65608d3ba2fe76daab42
Vaibhav Porwal <porwalv42@gmail.com>
2026-01-04 11:17:13 +0530

    Create empty repository

---
+++ app/src/main/java/github/porwalv42hue/budgetit/MainActivity.kt
@@ -0,0 +1,57 @@
+package github.porwalv42hue.budgetit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.porwalv42hue.budgetit.ui.theme.BudgetItTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BudgetItTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    floatingActionButton = {FloatingActionContainer()}
+                ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FloatingActionContainer() {
+    ElevatedButton(onClick = {}) {
+        Text(text = "Settings")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BudgetItTheme {
+        Greeting("Android")
+    }
+}
