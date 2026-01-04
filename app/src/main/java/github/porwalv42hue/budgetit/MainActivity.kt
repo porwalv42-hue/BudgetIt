4f8e0c87d4420e6450e214f01ba84c2809bb4efe
Vaibhav Porwal <porwalv42@gmail.com>
2026-01-04 13:19:47 +0530

    Add views in floatingActionButton

--- app/src/main/java/github/porwalv42hue/budgetit/MainActivity.kt
+++ app/src/main/java/github/porwalv42hue/budgetit/MainActivity.kt
@@ -4,14 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import github.porwalv42hue.budgetit.ui.theme.BudgetItTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +46,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     floatingActionButton = {FloatingActionContainer()}
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Content(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,23 +57,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FloatingActionContainer() {
-    ElevatedButton(onClick = {}) {
-        Text(text = "Settings")
+    var expanded by remember { mutableStateOf(false) }
+    val items = listOf("Income", "Household", "Investment", "Donation", "Trip", "Grooming")
+    var selectedIndex by remember { mutableStateOf(0) }
+    var amount by remember { mutableStateOf("0") }
+
+    Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.secondary,
+        shape = RoundedCornerShape(10.dp)).fillMaxWidth(0.9f)) {
+
+        Row(modifier = Modifier.padding(10.dp)) {
+
+            Column(modifier = Modifier.weight(6.5f)) {
+
+                TextField(
+                    value = amount, singleLine = true,
+                    onValueChange = { amount = it },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Box {
+                    ElevatedButton(onClick = { expanded = true },
+                        shape = RoundedCornerShape(4.dp),
+                        modifier = Modifier.fillMaxWidth()) {
+                        Row() {
+                            Text(items[selectedIndex])
+                            Icon(Icons.Default.ArrowDropDown,
+                                modifier = Modifier.width(30.dp).height(30.dp),
+                                contentDescription = "Drop Down Arrow")
+                        }
+                    }
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        items.forEachIndexed { index, s ->
+                            DropdownMenuItem(
+                                text = { Text(s) },
+                                onClick = {
+                                    selectedIndex = index
+                                    expanded = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            ElevatedButton(onClick = {},
+                modifier = Modifier.weight(3.5f).align(Alignment.CenterVertically)) {
+                Text("Add")
+            }
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Content(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Remaining Budget:",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BudgetItTheme {
-        Greeting("Android")
-    }
 }
